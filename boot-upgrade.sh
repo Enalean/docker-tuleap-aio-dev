f3722faa00413f59e33bb1f30a14d6b150ae502e
Benjamin Dauton <benjamin.dauton@enalean.com>
2017-01-19 09:45:53 +0100

    Remove the forgeupgrade update because it's very slow on docker for mac

--- boot-upgrade.sh
+++ boot-upgrade.sh
@@ -2,8 +2,5 @@
 
 set -ex
 
-# On start, ensure db is consistent with data (useful for version bump)
-/usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/codendi/forgeupgrade/config.ini update
-
 # Ensure system will be synchronized ASAP (once system starts)
 /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/src/utils/launch_system_check.php
