fca4f980004b423107e7126d1175f2fda68bd0e3
Benjamin Dauton <benjamin.dauton@enalean.com>
2017-01-19 12:10:29 +0100

    Fix boolean test on DO_NOT_LAUNCH_FORGEUPGRADE env variable

--- boot-upgrade.sh
+++ boot-upgrade.sh
@@ -3,6 +3,8 @@
 set -ex
 
 if [ "$DO_NOT_LAUNCH_FORGEUPGRADE" == true ] ; then
+    echo "Database may be inconsistent. You should run a forgeupgrade update."
+else
     # On start, ensure db is consistent with data (useful for version bump)
     /usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/codendi/forgeupgrade/config.ini update
 fi
