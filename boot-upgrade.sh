2055e82d77c8c321d2f6c37fcd27bf0a519ae467
Benjamin Dauton <benjamin.dauton@gmail.com>
2018-03-19 21:41:33 +0100

    Do not launch forgeupgrade update if DO_NOT_LAUNCH_FORGEUPGRADE variable is set to true

--- boot-upgrade.sh
+++ boot-upgrade.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
-# On start, ensure db is consistent with data (useful for version bump)
-/usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/codendi/forgeupgrade/config.ini update
+if [ "$DO_NOT_LAUNCH_FORGEUPGRADE" == true ] ; then
+    echo "Database may be inconsistent. You should run a forgeupgrade update."
+else
+    # On start, ensure db is consistent with data (useful for version bump)
+    /usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/codendi/forgeupgrade/config.ini update
+fi
 
 # Ensure system will be synchronized ASAP (once system starts)
 /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/src/utils/launch_system_check.php
